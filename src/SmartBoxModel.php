1e478d0571537a3d7edc0e94a1f620b68854cb86
Dan <damian.dan@me.com>
2016-02-23 18:15:11 +0000

    DoNoWhy (updates)

--- src/SmartBoxModel.php
+++ src/SmartBoxModel.php
@@ -9,4 +9,15 @@ class SmartBoxModel
         //TODO: Retrieve file from configuration file
         return file_put_contents(__DIR__ . '/../data/temp.txt', $value, LOCK_EX);
     }
+
+    public function getTempBySerial($sId)
+    {
+        $temp = exec('cat /sys/bus/w1/devices/' . $sId . '/w1_slave |grep t=');
+	$temp = explode('t=',$temp);
+	$temp = $temp[1] / 1000;
+	$temp = round($temp,2);
+
+	return $temp;
+
+    }
 }
--- web/index.php
+++ web/index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 // TODO: 1. Validate session
 // TODO: 2. Validate value
 
@@ -14,13 +15,12 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
-use Symfony\Component\HttpFoundation\Request; 
-use Symfony\Component\HttpFoundation\Response; 
-use Symfony\Component\HttpFoundation\JsonResponse; 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 $request = Request::createFromGlobals();
 
-$route = new Route('/submit/{parameters}', array('controller' => 'Helper\SmartBoxController', 'action'=>'index'));
+$route = new Route('/submit/{parameters}', array('controller' => 'Helper\SmartBoxController', 'action' => 'index'));
 $routes = new RouteCollection();
 $routes->add('route_name', $route);
 
@@ -33,16 +33,17 @@ $matcher = new UrlMatcher($routes, $context);
 
 $context->fromRequest(Request::createFromGlobals());
 $matcher = new UrlMatcher($routes, $context);
+try {
+    $attributes = $matcher->match($request->getPathInfo());
+    $controller = $attributes['controller'];
+    unset($attributes['controller']);
+    $response = call_user_func_array(array($controller, 'index'), array($request, $attributes));
+} catch (ResourceNotFoundException $e) {
+    $response = new JsonResponse('Not found!', JsonResponse::HTTP_NOT_FOUND);
+} catch (\Exception $e) {
+    // TODO: Add some logging in here
+    $response = new JsonResponse('An error occurred', 500);
+}
 
-    try {     
-       	$attributes = $matcher->match($request->getPathInfo());
-		$controller = $attributes['controller'];
-		unset($attributes['controller']);
-		$response = call_user_func_array(array($controller, 'index'), array($request, $attributes));
-    }catch (ResourceNotFoundException $e){
-        $response = new JsonResponse('Not found!', Response::HTTP_NOT_FOUND);
-    }catch (\Exception $e) {
-            // TODO: Add some logging in here
-            $response = new JsonResponse('An error occurred', 500);
-    }
+$response->headers->set('access-control-allow-origin', 'http://sb.imediat.eu');
 $response->send();
